d09a0f488ad1004bec7713bf67e87d6aa997df4e
Татьяна Воронцова <47387956+makovka2011@users.noreply.github.com>
2024-04-11 09:42:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let a = '223';
+let b = 5;
+var iceCream = 'nonchocolate';
+var myVariable = document.querySelector('h1');
+//var myVariable = true;
+//var myVariable = false;
+//var myArray = [1,'Tatyana','Alex',10];
+//var myVariable = document.querySelector('h1');
+
+//alert(a+b);
+//alert(!(b === 5));
+/*if (iceCream === 'chocolate') {
+	alert('I love chocolate');
+} else {
+	alert('mmm, but not cholate');
+}*/
+//alert('hello!');
+/*function multiply(num1,num2) {
+	var result = num1*num2;
+	return result;
+}*
